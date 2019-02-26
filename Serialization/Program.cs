4fac4315b3408750f114f325434bfd2a369b9d30
cagataykiziltan <45563744+cagataykiziltan@users.noreply.github.com>
2019-02-26 22:41:57 +0200

    First commit
    serialization first commit

---
+++ Serialization/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Serialization
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Message message = new Message
+            {
+                Sender = "Mustafa Cagatay KIZILTAN",
+                Receiver = "Sample Receiver",
+                MessageBody = "Sample  Message"
+            };
+
+
+            string serializedJsonMessage = Serializer.ToJson(message);
+            Message deserializedJsonMessage = Serializer.FromJson<Message>(serializedJsonMessage);
+
+            string serializedXmlMessage = Serializer.ToXml(message);
+            Message deserializedXmlMessage = Serializer.FromXml<Message>(serializedXmlMessage);
+
+            byte[] serializedBinaryMessage = Serializer.ToBinary(message);
+            Message deserializedBinaryMessage = Serializer.FromBinary<Message>(serializedBinaryMessage);
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+    
+    [Serializable]
+    public class Message
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string MessageBody { get; set; }
+    }
+}
---
+++ Serialization/Serializer.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Serialization
+{
+    public static class Serializer
+    {
+        public static string ToJson<T>(T thisObject)
+        {
+           return JsonConvert.SerializeObject(thisObject, new JsonSerializerSettings()
+            {
+                ContractResolver = new DefaultContractResolver(),
+                NullValueHandling = NullValueHandling.Include,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+
+            });
+
+        }
+
+        public static T FromJson<T>(string jsonString)
+        {
+            return (T)JsonConvert.DeserializeObject(jsonString, typeof(T), new JsonSerializerSettings()
+            {
+                ContractResolver = new DefaultContractResolver(),
+                NullValueHandling = NullValueHandling.Include,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+
+            });
+        }
+
+        public static string ToXml<T>(T thisObject)
+        {
+
+            var serializer = new XmlSerializer(typeof(T));
+            XmlDocument xmlDoc = new XmlDocument();
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                serializer.Serialize(memoryStream, thisObject);
+                memoryStream.Position = 0;
+                xmlDoc.Load(memoryStream);
+
+                return xmlDoc.InnerXml;
+            }
+
+        }
+
+        public static T FromXml<T>(string xmlString)
+        {
+            StringReader xmlReader = new StringReader(xmlString);
+            XmlSerializer deserializer = new XmlSerializer(typeof(T));
+
+            return (T)deserializer.Deserialize(xmlReader);
+        }
+
+        public static byte[] ToBinary(object thisObject)
+        {
+            using (var stream = new MemoryStream())
+           {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, thisObject);
+
+                return stream.ToArray();
+
+            }
+
+        }
+     
+        public static T FromBinary<T>(byte[] buffer)
+        {
+            using (var stream = new MemoryStream(buffer))
+            {
+                var formatter = new BinaryFormatter();
+
+                return (T)formatter.Deserialize(stream);
+
+            }
+        }
+
+    }
+
+}
